956e47b24b5fe023b96580c4c16898c35df9dd19
Tahir-DA-Ds <tahir.adeleye@gmail.com>
2024-01-23 04:41:34 +0100

    product controller update

--- controllers/productCtrl.js
+++ controllers/productCtrl.js
@@ -1,34 +1,63 @@
-const Product = require('../models/productModel')
-const asyncHandler = require("express-async-handler")
-const slugify = require("slugify")
+const Product = require("../models/productModel");
+const asyncHandler = require("express-async-handler");
+const slugify = require("slugify");
 
-const createProduct = asyncHandler(async(req, res)=>{
-    try {
-        const newProduct = await Product.create(req.body)
-        res.json(newProduct)
-    } catch (error) {
-        throw new Error(error)
+const createProduct = asyncHandler(async (req, res) => {
+  try {
+    if (req.body.title) {
+      req.body.slug = slugify(req.body.title);
     }
-})
+    const newProduct = await Product.create(req.body);
+    res.json(newProduct);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
 
-const getaProduct = asyncHandler(async(req, res)=>{
-    const {id} = req.params
-    try {
-        const findProduct = await Product.findById(id)
-        res.json(findProduct)
-    } catch (error) {
-        throw new Error(error)
+const updateProduct = asyncHandler(async (req, res) => {
+  const id = req.params._id;
+  console.log(id, "here");
+  try {
+    if (req.body.title) {
+      req.body.slug = slugify(req.body.title);
     }
-} )
+    const productUpdate = await Product.findOneAndUpdate({id}, req.body, {
+        new: true,
+    });
+    console.log(productUpdate);
+    res.json(productUpdate);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
 
-const getAllproducts = asyncHandler(async (req, res)=>{
-    try {
-        const allProducts = await Product.find({})
-        res.json(allProducts)
+const deleteProduct = asyncHandler(async (req, res) => {
+    const id = req.params._id;
+    try{
+      const deleteproduct = await Product.findOneAndDelete({id})
+      res.json(`product with id:${id} deleted`)
     } catch (error) {
-        throw new Error(error)
-        
+      throw new Error(error);
     }
-})
+  });
 
-module.exports = {createProduct, getaProduct, getAllproducts}
+const getaProduct = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  try {
+    const findProduct = await Product.findById(id);
+    res.json(findProduct);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+const getAllproducts = asyncHandler(async (req, res) => {
+  try {
+    const allProducts = await Product.find({});
+    res.json(allProducts);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+module.exports = { createProduct, getaProduct, getAllproducts, updateProduct, deleteProduct};
--- routes/productRouter.js
+++ routes/productRouter.js
@@ -1,11 +1,17 @@
-const express = require("express")
-const router = express.Router()
-const {createProduct, getaProduct, getAllproducts} = require('../controllers/productCtrl')
+const express = require("express");
+const router = express.Router();
+const {
+  createProduct,
+  getaProduct,
+  getAllproducts,
+  updateProduct,
+  deleteProduct,
+} = require("../controllers/productCtrl");
+const { isAdmin, authMiddleware } = require("../middlewares/authMiddleware");
+router.post("/", authMiddleware, isAdmin, createProduct);
+router.get("/:id", getaProduct);
+router.put("/:id", authMiddleware, isAdmin, updateProduct);
+router.delete("/:id", authMiddleware, isAdmin, deleteProduct);
+router.get("/", getAllproducts);
 
-router.post('/', createProduct)
-router.get('/:id', getaProduct)
-router.get('/', getAllproducts)
-
-
-
-module.exports = router
+module.exports = router;
